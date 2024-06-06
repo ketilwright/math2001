1bc03f6769234c21b7fe754bb8c8931d508949fa
ketilwright <ketilwright@icloud.com>
2024-06-06 13:29:20 -0700

    01_parity.lean all but last

--- Math2001/03_Parity_and_Divisibility/01_Parity.lean
+++ Math2001/03_Parity_and_Divisibility/01_Parity.lean
@@ -89,42 +89,120 @@ example (n : ℤ) : Even (n ^ 2 + n + 4) := by
 
 
 example : Odd (-9 : ℤ) := by
-  sorry
+  use - 5
+  numbers
 
 example : Even (26 : ℤ) := by
-  sorry
+  use 13
+  ring
 
 example {m n : ℤ} (hm : Odd m) (hn : Even n) : Odd (n + m) := by
-  sorry
+  obtain ⟨a, ha⟩ := hm
+  obtain ⟨b, hb⟩ := hn
+  use (a + b)
+  rewrite [ha, hb];
+  ring
 
 example {p q : ℤ} (hp : Odd p) (hq : Even q) : Odd (p - q - 4) := by
-  sorry
+  obtain ⟨a, ha⟩ := hp
+  obtain ⟨b, hb⟩ := hq
+  use a - b - 2
+  rewrite [ha, hb]
+  ring
 
 example {a b : ℤ} (ha : Even a) (hb : Odd b) : Even (3 * a + b - 3) := by
-  sorry
+  obtain ⟨x, hx⟩ := ha
+  obtain ⟨y, hy⟩ := hb
+  use 3 * x + y - 1
+  rewrite [hx, hy]
+  ring
+
 
 example {r s : ℤ} (hr : Odd r) (hs : Odd s) : Even (3 * r - 5 * s) := by
-  sorry
+  obtain ⟨a, ha⟩ := hr
+  obtain ⟨b, hb⟩ := hs
+  use 3 * a - 5 * b - 1
+  rewrite [ha, hb]
+  ring
 
 example {x : ℤ} (hx : Odd x) : Odd (x ^ 3) := by
-  sorry
+  dsimp [Odd] at *
+  obtain ⟨a, ha⟩ := hx
+  use 4 * a ^ 3 + 6 * a ^ 2 + 3 * a
+  rewrite [ha]
+  ring
 
 example {n : ℤ} (hn : Odd n) : Even (n ^ 2 - 3 * n + 2) := by
-  sorry
+  dsimp [Odd, Even] at *
+  obtain ⟨a, ha⟩ := hn
+  rewrite [ha]
+  use 2 * a ^ 2 - a
+  ring
 
 example {a : ℤ} (ha : Odd a) : Odd (a ^ 2 + 2 * a - 4) := by
-  sorry
+  dsimp [Odd] at *
+  obtain ⟨x, hx⟩ := ha
+  rewrite [hx]
+  use 2 * x ^ 2 + 4 * x - 1
+  ring
 
 example {p : ℤ} (hp : Odd p) : Odd (p ^ 2 + 3 * p - 5) := by
-  sorry
+  dsimp [Odd] at *
+  obtain ⟨x, hx⟩ := hp
+  rewrite [hx]
+  use 2 * x ^ 2 + 5 * x - 1
+  ring
 
 example {x y : ℤ} (hx : Odd x) (hy : Odd y) : Odd (x * y) := by
-  sorry
+  dsimp [Odd] at *
+  obtain ⟨a, ha⟩ := hx
+  obtain ⟨b, hb⟩ := hy
+  rewrite [ha, hb]
+  use 2 * a * b + a + b
+  ring
 
 example (n : ℤ) : Odd (3 * n ^ 2 + 3 * n - 1) := by
-  sorry
+  -- The first 2 terms of 3n² + 3n - 1 = 3n(n+1) which is even
+  have h1: Even (3 * n ^ 2 + 3 * n) := by
+    have h2: 3 * n ^ 2 + 3 * n = 3 * n * (n + 1) := by ring
+    rewrite [h2]
+    -- n must be even or odd
+    obtain h3 | h3 := Int.even_or_odd n
+    -- suppose n is even
+    obtain ⟨j, hj⟩ := h3
+    rewrite [hj]
+    use 6 * j ^ 2 + 3 * j
+    ring
+    -- suppose n is odd
+    obtain ⟨j, hj⟩ := h3
+    use 6 * j ^ 2 + 9 * j + 3
+    rewrite [hj]
+    ring
+    done
+  -- Since 3n² + 3n is even, 3n² + 3n - 1 is odd
+  obtain ⟨x, hx⟩ := h1
+  rewrite [hx]
+  use x - 1
+  ring
+
 
 example (n : ℤ) : ∃ m ≥ n, Odd m := by
-  sorry
+  -- n is either even or odd
+  obtain h1 | h1 := Int.even_or_odd n
+  -- suppose n is even, and choose k | n = 2k
+  obtain ⟨k, hk⟩ := h1
+  -- the n + 1 = 2 k + 1 is a "witness"
+  use n + 1
+  constructor
+  apply le.intro 1 rfl
+  use k; rewrite [hk]; ring
+  -- suppose n is odd, and choose k ∣ n = 2k + 1
+  obtain ⟨k, hk⟩ := h1
+  --  n is a witness that k is odd
+  use n
+  constructor
+  apply Int.le_refl n
+  use k; apply hk
+
 example (a b c : ℤ) : Even (a - b) ∨ Even (a + c) ∨ Even (b - c) := by
   sorry
