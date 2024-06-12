732fcf5a01108eaf0e2fe6cc248ccca2b1726ccf
ketilwright <ketilwright@icloud.com>
2024-06-12 15:25:44 -0700

    02_Iff.lean checkpoint

--- Math2001/04_Proofs_with_Structure_II/02_Iff.lean
+++ Math2001/04_Proofs_with_Structure_II/02_Iff.lean
@@ -10,11 +10,11 @@ namespace Int
 
 example {a : ℚ} : 3 * a + 1 ≤ 7 ↔ a ≤ 2 := by
   constructor
-  · intro h
+  · intro h -- (→)
     calc a = ((3 * a + 1) - 1) / 3 := by ring
       _ ≤ (7 - 1) / 3 := by rel [h]
       _ = 2 := by numbers
-  · intro h
+  · intro h -- (←)
     calc 3 * a + 1 ≤ 3 * 2 + 1 := by rel [h]
       _ = 7 := by numbers
 
@@ -38,12 +38,15 @@ example {n : ℤ} : 8 ∣ 5 * n ↔ 8 ∣ n := by
 theorem odd_iff_modEq (n : ℤ) : Odd n ↔ n ≡ 1 [ZMOD 2] := by
   constructor
   · intro h
-    obtain ⟨k, hk⟩ := h
+    obtain ⟨k, hk⟩ := h -- n = 2 * k + 1
     dsimp [Int.ModEq]
     dsimp [(· ∣ ·)]
     use k
     addarith [hk]
-  · sorry
+  · intro h -- n ≡ 1 [ZMOD 2]
+    obtain ⟨k, hk⟩ := h -- n - 1 = 2 * k
+    use k
+    addarith [hk]
 
 theorem even_iff_modEq (n : ℤ) : Even n ↔ n ≡ 0 [ZMOD 2] := by
   constructor
@@ -53,13 +56,98 @@ theorem even_iff_modEq (n : ℤ) : Even n ↔ n ≡ 0 [ZMOD 2] := by
     dsimp [(· ∣ ·)]
     use k
     addarith [hk]
-  · sorry
+  · intro h
+    obtain ⟨k, hk⟩ := h
+    use k
+    addarith [hk]
+
 
 example {x : ℝ} : x ^ 2 + x - 6 = 0 ↔ x = -3 ∨ x = 2 := by
-  sorry
+  constructor
+  ·
+    intro h -- assume x² + x - 6 - 0
+    have h1:=
+      calc (x + 3) * (x - 2)
+        _ = x ^ 2 + x - 6 := by ring
+        _ = 0 := h
 
+    obtain h2 | h2 := eq_zero_or_eq_zero_of_mul_eq_zero h1
+    left; addarith [h2];
+    right; addarith [h2]
+  ·
+    intro h
+    obtain h2 | h2 := h
+    calc x ^ 2 + x - 6
+      _ = (-3) ^ 2 + (-3) - 6 := by rw [h2]
+      _ = 0 := by numbers
+    calc x ^ 2 + x - 6
+      _ = 2 ^ 2 + 2 - 6 := by rw [h2]
+      _ = 0 := by numbers
+
+
+-- Int.mul_pos_of_neg_of_neg
+-- Int.mul_lt_mul_of_neg_right
 example {a : ℤ} : a ^ 2 - 5 * a + 5 ≤ -1 ↔ a = 2 ∨ a = 3 := by
-  sorry
+  constructor
+  · -- ( → )
+  -- Suppose a² -5a + 5 ≤ 1
+    intro h
+  -- then (2a - 5)² ≤ 1 = 1²
+    have h1:=
+      calc (2 * a - 5) ^ 2
+        _ = 4 * (a ^ 2 - 5 * a + 5) + 5 := by ring
+        _ ≤ 4 * (-1) + 5 := by rel [h]
+        _ = 1 ^ 2 := by ring
+    /-
+      “And” hypotheses turn up relatively rarely in the wild
+      One situation in which one might occur is when a single hypothesis has
+      two natural consequences, which are paired together in a lemma
+      For example, if x² ≤ y² for some positive number y, then one can draw
+      the conclusion  -y ≤ x ≤ y ( use abs_le_of_sq_le_sq')
+    -/
+    have h2: (0: ℤ) ≤ 1 ^ 2 := by numbers
+    obtain ⟨h3, h4⟩ := abs_le_of_sq_le_sq' h1 h2
+
+    have h5:=
+      calc 2 * a
+        _ = 2 * a - 5 + 5 := by ring
+        _ ≥ -1 + 5 := by rel [h3]
+        _ = 2 * 2 := by ring
+    have h6: (0: ℤ) < 2 := by numbers
+    have h7: 2 ≤ a := le_of_mul_le_mul_left h5 h6
+    have h8:=
+      calc 2 * a
+        _ = 2 * a - 5 + 5 := by ring
+        _ ≤ 1 + 5 := by rel [h4]
+        _ = 2 * 3 := by ring
+    have h9: a ≤ 3 := le_of_mul_le_mul_left h8 h6
+    obtain h10 | h10 := lt_or_eq_of_le h7
+    ·
+      -- suppose a > 2
+      right; apply Int.le_antisymm h9 h10
+    ·
+      -- a = 2
+      left;
+      apply h10.symm
+  · -- ( ← )
+    intro h11
+    obtain h12 | h12 := h11
+    ·
+      -- suppose a = 2
+      calc a ^ 2 - 5 * a + 5
+        _ = 2 ^ 2 - 5 * 2 + 5 := by rw [h12]
+        _ = -1 := by ring
+        _ ≤ -1 := by extra
+    ·
+      -- suppose a = 3
+      calc a ^ 2 - 5 * a + 5
+        _ = 3 ^ 2 - 5 * 3 + 5 := by rw [h12]
+        _ = -1 := by ring
+        _ ≤ -1 := by extra
+
+
+
+
 
 example {n : ℤ} (hn : n ^ 2 - 10 * n + 24 = 0) : Even n := by
   have hn1 :=
