f7bd8d474b8d16e5f77b28b2650c611bb212aae5
ketilwright <ketilwright@icloud.com>
2024-06-16 14:12:54 -0700

    rewrite ∃! r : ℤ, 0 ≤ r ∧ r < 5 ∧ 14 ≡ r [ZMOD 5] uniqueness bit more pedantically

--- Math2001/04_Proofs_with_Structure_II/03_Exists_Unique.lean
+++ Math2001/04_Proofs_with_Structure_II/03_Exists_Unique.lean
@@ -81,7 +81,6 @@ example {x : ℚ} (hx : ∃! a : ℚ, a ^ 2 = x) : x = 0 := by
     _ = 0 ^ 2 := by rw [h2]
     _ = 0 := by ring
 
-
 example : ∃! r : ℤ, 0 ≤ r ∧ r < 5 ∧ 14 ≡ r [ZMOD 5] := by
   use 4
   dsimp
@@ -107,6 +106,53 @@ example : ∃! r : ℤ, 0 ≤ r ∧ r < 5 ∧ 14 ≡ r [ZMOD 5] := by
   cancel 5 at h43 -- q < 3
   interval_cases q
   addarith [hr3]
+
+/-
+  I found the preceding proof's uniqueness bit too difficult to follow,
+  so I rewrote that part more pedantically
+-/
+example : ∃! r : ℤ, 0 ≤ r ∧ r < 5 ∧ 14 ≡ r [ZMOD 5] := by
+  use 4
+  dsimp
+  constructor
+  · constructor
+    · numbers
+    constructor
+    · numbers
+    use 2
+    numbers
+  -- Let r ∈ Z be arbitrary
+  --  Suppose 0 ≤ r < 5 and 14 ≡₅ r
+  intro r hr
+  obtain ⟨h1, htmp⟩ := hr
+  obtain ⟨h2, h3⟩ := htmp
+  --    Since r < 5, 14 - r > 0
+  have h4:=
+    calc 14 - r
+      _ > 14 - 5 := by rel [h2]
+      _ > 0 := by numbers
+  --    Since 0 ≤ r < 5, 5 < 14 - r
+  have h5: 5 < 14 - r := by addarith [h1, h2, h4]
+  --    Since 14 ≡₅ r we can choose q with 14 - r = 5q
+  obtain ⟨q, hq⟩ := h3
+  --    Since 5q = 14 - r > 5, thus q > 1
+  have h6:=
+    calc 5 * q
+      _ = 14 - r := by rw [hq]
+      _ > 5 := h5
+      _ = 5 * 1 := by ring
+  cancel 5 at h6
+  --    Since 5q = 14 - r, and r ≥ 0, 5q < 15, so q < 3
+  have h7:=
+    calc 5 * q
+      _ = 14 - r := by rw [hq]
+      _ ≤ 14 - 0 := by rel [h1]
+      _ < 5 * 3 := by numbers
+  cancel 5 at h7
+  -- Since 1 < q < 3, q = 2,
+  interval_cases q
+  -- Substituting q = 2 in 14 - r = 5q, thus r = 4 □
+  addarith [hq]
 
 /-! # Exercises -/
 
@@ -135,5 +181,98 @@ example : ∃! n : ℕ, ∀ a, n ≤ a := by
     intro y hy
     calc y = 0 := Nat.le_zero.mp (hy 0)
 
+
+
 example : ∃! r : ℤ, 0 ≤ r ∧ r < 3 ∧ 11 ≡ r [ZMOD 3] := by
-  sorry
+  use 2
+  dsimp
+  constructor
+  ·
+    constructor
+    ·
+      numbers -- 0 ≤ 2
+    ·
+      constructor
+      ·
+        numbers -- 2 < 3
+      · -- 11 ≡₃ 2
+        use 3
+        numbers
+  ·
+    intro y hy
+    obtain ⟨hy1, hy2, x, hy3⟩ := hy
+
+    have h4:=
+      calc 3 * 1
+        --_ < 3 * 1 := by numbers
+        _ < 11 - y := by addarith [hy2]
+        _ = 3 * x := hy3
+    cancel 3 at h4 -- 1 < x
+    have h5:=
+      calc 3 * x
+        _ = 11 - y := by rw [hy3]
+        _ < 3 * 4 := by addarith [hy1]
+    cancel 3 at h5 -- x < 4
+
+    have h7:=
+      calc 11 - y
+        _ > 11 - 3 := by rel [hy2]
+
+
+    -- cancel 1 at h6
+    -- interval_cases x
+    -- addarith [hy3]
+    -- have h4:=
+    --   calc 2 * 1
+    --     _ < 3 * 1 := by numbers
+    --     _ < 11 - y := by addarith [hy2]
+
+    -- have h4:=
+    --   calc 3 * 1
+    --     _ < 11 - y := by addarith [hy2]
+    --     _ = 3 * x := hy3
+    -- cancel 3 at h4
+    -- have h5:=
+    --   calc 3 * x
+    --     _ = 11 - y := by rw [hy3]
+    --     _ < 3 * 4  := by addarith [hy1]
+
+    -- cancel 3 at h5
+    -- interval_cases x
+
+
+
+
+    --have h4: y = 11 - 3 * x := by addarith [hy3]
+  --   obtain ⟨h1, hConj⟩ := hy -- h1: 0 ≤ y
+  --   obtain ⟨h2, h3⟩ := hConj -- h2: y < 3
+  --   dsimp [Int.ModEq] at h3
+  --   --dsimp [. ∣ .] at h3
+  --   -- we consider the 3 possible values of y
+  --   /-
+  --   lemma Int.existsUnique_modEq_lt (a b : ℤ) (h : 0 < b) :
+  -- ∃! r : ℤ, 0 ≤ r ∧ r < b ∧ a ≡ r [ZMOD b] :=
+  --   -/
+  --   have h42: 3 ∣ (11 - 2) := by use 3; numbers
+
+
+    -- interval_cases y
+    -- ·
+    --   -- Suppose y = 2
+    --   -- have h42: 3 ∣ (11 - 2) := by use 3; numbers
+
+    --   --obtain ⟨x, hx⟩ := h3
+
+    --   sorry
+    -- ·
+    --   sorry
+    -- ·
+    --   sorry
+    -- -- dsimp [Int.ModEq] at h3
+
+    -- -- dsimp [. ∣ .] at h3
+
+    -- -- obtain ⟨x, h4⟩ := h3 -- 11 - y = 3 * x
+
+
+    sorry
