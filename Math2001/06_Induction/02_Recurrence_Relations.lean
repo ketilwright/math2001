7b7cb3d93343244585bf8b6aa709a375950c9efc
ketilwright <ketilwright@icloud.com>
2024-07-04 15:08:06 -0700

    02_Recurrence_Relations examples done

--- Math2001/06_Induction/02_Recurrence_Relations.lean
+++ Math2001/06_Induction/02_Recurrence_Relations.lean
@@ -19,7 +19,7 @@ def b : ℕ → ℤ
   | n + 1 => b n ^ 2 - 2
 
 
-#eval b 7 -- infoview displays `316837008400094222150776738483768236006420971486980607`
+--#eval b 7 -- infoview displays `316837008400094222150776738483768236006420971486980607`
 
 
 example (n : ℕ) : Odd (b n) := by
@@ -35,6 +35,26 @@ example (n : ℕ) : Odd (b n) := by
       _ = (2 * x + 1) ^ 2 - 2 := by rw [hx]
       _ = 2 * (2 * x ^ 2 + 2 * x - 1) + 1 := by ring
 
+/-
+  You might like to try giving another proof using the modular-arithmetic
+  characterization of parity; this will work both in text and in Lean.
+-/
+example (n : ℕ) : b n ≡ 1 [ZMOD 2] := by
+  dsimp [Int.ModEq, . ∣ .]
+  simple_induction n with k hk
+  ·
+    use 1;  rw [b]; numbers
+  ·
+    obtain ⟨x, hx⟩ := hk
+    have h1: b k = 2 * x + 1 := by addarith [hx]
+    have h2: b (k + 1) = (b k) ^ 2 - 2 := by rfl
+    use 2 * x ^ 2 + 2 * x - 1
+    calc b (k + 1) - 1
+      _ = (b k) ^ 2 - 2 - 1 := by rw [h2]
+      _ = (2 * x + 1) ^ 2 - 2 - 1 := by rw [h1]
+      _ = 4 * x ^ 2 + 4 * x - 2 := by ring
+      _ = 2 * (2 * x ^ 2 + 2 * x - 1) := by ring
+
 
 def x : ℕ → ℤ
   | 0 => 5
@@ -42,12 +62,20 @@ def x : ℕ → ℤ
 
 
 example (n : ℕ) : x n ≡ 1 [ZMOD 4] := by
+  have x0: x 0 = 5 := by rw [x]
   simple_induction n with k IH
-  · -- base case
-    sorry
+  · -- base case is trivial
+    dsimp [Int.ModEq]; rw [x]; use 1; numbers
   · -- inductive step
-    sorry
+    -- Suppose xₖ ≡₄ 1
+    -- Then since xₖ₊₁ = 2 ⬝ xₖ - 1, 2 ≡₄ 2 and -1 ≡₄ -1 we can use ModEq.mul and add to
+    -- see that we have xₖ₊₁ ≡₄ (2 ⬝ 1 - 1)
+    calc x (k + 1)
+      _ = 2 * (x k) - 1 := by rw [x]
+      _ ≡ (2 * 1 - 1) [ZMOD 4] := Int.ModEq.add (Int.ModEq.mul (by extra) IH) (by extra)
+      _ = 1 := by numbers
 
+-- closed form
 example (n : ℕ) : x n = 2 ^ (n + 2) + 1 := by
   simple_induction n with k IH
   · -- base case
@@ -56,7 +84,7 @@ example (n : ℕ) : x n = 2 ^ (n + 2) + 1 := by
   · -- inductive step
     calc x (k + 1) = 2 * x k - 1 := by rw [x]
       _ = 2 * (2 ^ (k + 2) + 1) - 1 := by rw [IH]
-      _ = 2 ^ ((k + 1) + 2) + 1 := by ring
+      _ = 2 ^ (k + 1 + 2) + 1 := by ring
 
 
 def A : ℕ → ℚ
@@ -83,23 +111,63 @@ def factorial : ℕ → ℕ
 
 notation:10000 n "!" => factorial n
 
+/-
+  Show that every natural number d with 1 ≤ d < n is a factor of n!
+-/
+-- protected theorem pos_iff_ne_zero : 0 < n ↔ n ≠ 0 := ⟨ne_of_gt, Nat.pos_of_ne_zero⟩
 
 example (n : ℕ) : ∀ d, 1 ≤ d → d ≤ n → d ∣ n ! := by
   simple_induction n with k IH
-  · -- base case
+  · -- base case: Suppose k = 0
     intro k hk1 hk
     interval_cases k
+    /-
+        My OC suggests another possible approach, which doesn't rely
+        on super clever use of interval_cases
+
+    apply Nat.eq_zero_of_le_zero at hk
+    -- Since 1 ≤ k either 1 < k or 1 = k
+    obtain h1 | h1 := lt_or_eq_of_le hk1
+    · -- We can't have 1 < k since k = 0
+      have h2: ¬ 1 < 0 := by numbers
+      rw [hk] at h1; contradiction
+    ·  -- Thus k = 1, and since 0! = 1, we are done
+      rw [factorial, h1]
+    -/
+
   · -- inductive step
     intro d hk1 hk
+    -- Since d ≤ k + 1, either d = k + 1 or d < k + 1
     obtain hk | hk : d = k + 1 ∨ d < k + 1 := eq_or_lt_of_le hk
-    · -- case 1: `d = k + 1`
-      sorry
-    · -- case 2: `d < k + 1`
-      sorry
+    · -- case 1: suppose d = k + 1
+      use (k !); rw [factorial, hk];
+    · -- case 2: suppose d < k + 1
+      -- The d ≤ k, and since 1 ≤ d, from the induction hypothesis, d ∣ k !
+      rw [Nat.lt_succ] at hk
+      -- Since d ∣ k!, there is x with k! = d * x
+      obtain ⟨x, hx⟩ := IH d hk1 hk
+      use (k + 1) * x
+      calc (k + 1)!
+      --  Since (k + 1) ! = (k + 1) ⬝ k!
+        _ = (k + 1) * k ! := by rw [factorial]
+      --  and k! = dx, we are done.
+        _ = (k + 1) * (d * x) := by rw [hx]
+        _ = d * ((k + 1) * x) := by ring
+
+-- this theorem is dvd_factorial
 
 example (n : ℕ) : (n + 1)! ≥ 2 ^ n := by
-  sorry
-
+  simple_induction n with k hk
+  · -- base case: n = 0
+    -- we have (0 + 1)! = (0 + 1) ⬝ 0! = 1 ≥ 2⁰ = 1
+    rw [factorial, factorial]; extra
+  · -- inductive step
+    -- Suppose (k + 1)! ≥ 2ᵏ
+    calc (k + 1 + 1)!
+      _ = (k + 2) * (k + 1) ! := by rw [factorial]
+      _ ≥ (k + 2) * 2 ^ k := by rel [hk]
+      _ = k * 2 ^ k + 2 ^ (k + 1) := by ring
+      _ ≥ 2 ^ (k + 1) := by extra
 
 /-! # Exercises -/
 
