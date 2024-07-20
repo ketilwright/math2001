24b585b16725b038ed0353a74e051e08041c394d
ketilwright <ketilwright@icloud.com>
2024-07-20 11:56:20 -0700

    pascal_symm with induction

--- Math2001/06_Induction/05_Pascal.lean
+++ Math2001/06_Induction/05_Pascal.lean
@@ -42,7 +42,6 @@ termination_by _ a b => a + b
 theorem pascal_eq (a b : ℕ) : pascal a b * a ! * b ! = (a + b)! := by
   match a, b with
   | a, 0 =>
-    -- it appears _ is just a. Why?
     calc pascal _ 0 * a ! * 0! = 1 * a ! * 0! := by rw [pascal]
       _ = 1 * a ! * 1 := by rw [factorial]
       _ = (a + 0)! := by ring
@@ -78,8 +77,6 @@ example (a b : ℕ) : (pascal a b : ℚ) = (a + b)! / (a ! * b !) := by
   calc pascal a b * (a ! * b !) = pascal a b * a ! * b ! := by ring
     _ = (a + b)! := by apply pascal_eq
 
--- #eval (pascal 6 4 : ℚ) = (10 !) / (6 ! * 4 !) -- true
-
 /-! # Exercises -/
 
 
@@ -92,32 +89,27 @@ theorem pascal_symm (m n : ℕ) : pascal m n = pascal n m := by
   | 0, b + 1 =>
     rw [pascal, pascal]
   | a + 1, b + 1 =>
-    -- pascal (a + 1) b + pascal a (b + 1)
-    -- ihs not used :(. Does this qualify
-    -- as an inductive proof?
-    have ih1: _ := pascal_symm (a + 1) b
-    have ih2: _ := pascal_symm a ( b + 1)
-    have h1:=
-      calc (pascal (a + 1) (b + 1)) * ((a + 1) !) * ((b + 1) !)
-        _ = ((a + 1) + (b + 1)) ! := pascal_eq (a + 1) (b + 1)
-        _ = ((b + 1) + (a + 1)) ! := by ring
-        _ = (pascal (b + 1) (a + 1)) * ((b + 1) !) * ((a + 1) !) := by rw [pascal_eq]
-        _ = (pascal (b + 1) (a + 1)) * ((a + 1) !) * ((b + 1) !) := by ring
 
-    cancel ((b + 1) !) at h1
-    cancel ((a + 1) !) at h1
+    have ih1: pascal (a + 1) b = pascal b (a + 1) := pascal_symm (a + 1) b
+    have ih2: pascal a (b + 1) = pascal (b + 1) a := pascal_symm a ( b + 1)
+
+    calc pascal (a + 1) (b + 1)
+      _ = pascal (a + 1) b + pascal a (b + 1) := by rw [pascal]
+      _ = pascal a (b + 1) + pascal (a + 1) b := by rw [add_comm]
+      _ = pascal (b + 1) a + pascal b (a + 1) := by rw [ih1, ih2]
+      _ = pascal (b + 1) (a + 1) := by rw [pascal]
 
 termination_by _ a b => a + b
 
--- I don't get why we need this "match blah blah" at all.
+-- I do not see the need for inductive reasoning in the preceding proof.
 -- Why not just this?
 theorem pascal_symm' (m n : ℕ) : pascal m n = pascal n m := by
   have h1:=
-      calc (pascal m n) * (m !) * (n !)
-        _ = (m + n) ! := pascal_eq m n
-        _ = (n + m) ! := by ring
-        _ = (pascal n m) * (n !) * (m !) := by rw [pascal_eq]
-        _ = (pascal n m) * (m !) * (n !) := by ring
+    calc (pascal m n) * (m !) * (n !)
+      _ = (m + n) ! := pascal_eq m n
+      _ = (n + m) ! := by rw [add_comm]
+      _ = (pascal n m) * (n !) * (m !) := by rw [pascal_eq]
+      _ = (pascal n m) * (m !) * (n !) := by ring
 
   cancel (n !) at h1; cancel (m !) at h1
 
