76b36be284837cff1786c93a6247eb830c4adfc1
ketilwright <ketilwright@icloud.com>
2024-07-26 14:56:04 -0700

    some comments on inf_many_primes

--- Math2001/07_Number_Theory/01_Infinitely_Many_Primes.lean
+++ Math2001/07_Number_Theory/01_Infinitely_Many_Primes.lean
@@ -5,16 +5,20 @@ math2001_init
 
 namespace Nat
 
+#check dvd_factorial
 
 example (N : ℕ) : ∃ p ≥ N, Prime p := by
   have hN0 : 0 < N ! := by apply factorial_pos
   have hN2 : 2 ≤ N ! + 1 := by addarith [hN0]
   -- `N! + 1` has a prime factor, `p`
   obtain ⟨p, hp, hpN⟩ : ∃ p : ℕ, Prime p ∧ p ∣ N ! + 1 := exists_prime_factor hN2
-  have hp2 : 2 ≤ p
+
+  have hp2 : 2 ≤ p --term mode!
   · obtain ⟨hp', hp''⟩ := hp
     apply hp'
-  obtain ⟨k, hk⟩ := hpN
+
+  obtain ⟨k, hk⟩ := hpN -- N ! + 1 = p * k
+
   match k with
   | 0 => -- if `k` is zero, contradiction
     have k_contra :=
@@ -23,26 +27,43 @@ example (N : ℕ) : ∃ p ≥ N, Prime p := by
       _ = 0 := by ring
     numbers at k_contra
   | l + 1 => -- so `k = l + 1` for some `l`
+
     -- the key fact: `p` is not a factor of `N!`
+    -- ie. since p divides n ! + 1, it cannot divide n!
+    -- a job for not_dvd_of_exists_lt_and_lt
     have key : ¬ p ∣ (N !)
     · apply Nat.not_dvd_of_exists_lt_and_lt (N !)
       use l
       constructor
-      · have :=
-        calc p * l + p = p * (l + 1) := by ring
+      · -- prove p ⬝ l < N!
+        have :=
+        calc p * l + p
+          _ = p * (l + 1) := by ring
           _ = N ! + 1 := by rw [hk]
-          _ < N ! + p := by addarith [hp2]
-        addarith [this]
+          _ < N ! + p := by addarith [hp2] -- since 2 ≤ p
+        addarith [this] -- too clever for me
       · calc N ! < N ! + 1 := by extra
           _ = p * (l + 1) := by rw [hk]
     -- so `p` is a prime number greater than or equal to `N`, as we sought
     use p
     constructor
-    · obtain h_le | h_gt : p ≤ N ∨ N < p := le_or_lt p N
-      · have : p ∣ (N !)
-        · apply dvd_factorial
-          · extra
-          · addarith [h_le]
+    · -- prove p ≥ N
+      -- Either p ≤ N or N < 0
+      obtain h_le | h_gt : p ≤ N ∨ N < p := le_or_lt p N
+      · -- suppose p ≤ N
+        -- Then by the definition of factorial, p divides N!
+        have : p ∣ (N !)
+        ·
+          -- Nat.dvd_factorial {m n : ℕ} (a✝ : 0 < m) (a✝¹ : m ≤ n) : m ∣ n !
+          apply dvd_factorial
+          · -- 0 < p
+            -- Since 2 ≤ p, 0 < p
+            extra
+          · -- suppose p ≤ N. Why then, p ≤ n
+            apply h_le -- addarith [h_le]
+        -- But p doesn't divide N, so we can eliminate the case
+        -- that p ≤ N
         contradiction
-      · addarith [h_gt]
+      · -- Suppose N < p. Then we have p ≥ N
+        addarith [h_gt]
     · apply hp
